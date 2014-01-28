db575828ae711d0068538fccd537eb1726e1daaa
Kaj-Michael Lang <milang@tal.org>
2014-01-28 21:06:56 +0200

    Make translation loading work

--- harbour-yradio.pro
+++ harbour-yradio.pro
@@ -14,6 +14,21 @@ SOURCES += src/harbour-yradio.cpp src/settings.cpp
 
 HEADERS += src/settings.h
 
+# lupdate src/*.{cpp,h} qml/*.{qml,js} -ts nls/*.ts
+
+CODECFORTR = UTF-8
+TRANSLATIONS += \
+    nls/yradio.en.ts \
+    nls/yradio.sv.ts \
+    nls/yradio.fi.ts
+
+updateqm.input = TRANSLATIONS
+updateqm.output = ${QMAKE_FILE_PATH}/${QMAKE_FILE_BASE}.qm
+updateqm.commands = lrelease ${QMAKE_FILE_IN} -qm ${QMAKE_FILE_OUT}
+updateqm.CONFIG += no_link
+QMAKE_EXTRA_COMPILERS += updateqm
+PRE_TARGETDEPS += compiler_updateqm_make_all
+
 OTHER_FILES += qml/*.qml \
     qml/cover/*.qml \
     qml/pages/*.qml \
@@ -21,5 +36,12 @@ OTHER_FILES += qml/*.qml \
     qml/*.js \
     rpm/harbour-yradio.spec \
     rpm/harbour-yradio.yaml \
-    harbour-yradio.desktop
+    harbour-yradio.desktop \
+    qml/pages/ProgramPage.qml \
+    qml/SongItem.qml \
+    qml/ProgramsModel.qml \
+    qml/pages/ChannelPage.qml
+
+RESOURCES += \
+    nls.qrc
 
--- src/harbour-yradio.cpp
+++ src/harbour-yradio.cpp
@@ -28,9 +28,7 @@
   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#ifdef QT_QML_DEBUG
 #include <QtQuick>
-#endif
 
 #include <QTranslator>
 #include <QTextCodec>
@@ -47,24 +45,24 @@ int main(int argc, char *argv[])
     Settings *settings;
 
     app->setApplicationName("harbour-yradio");
-    app->setApplicationVersion("1.0.0");
+    app->setApplicationVersion("1.0.2");
     app->setOrganizationDomain("org.tal");
     app->setOrganizationName("TalOrg");
 
     // QTextCodec::setCodecForTr(QTextCodec::codecForName("utf8"));
     QString locale(QLocale::system().name());
 
-    if (translator.load("harbour-yradio." + locale, ":/nls")) {
-        qDebug() << "Locale: " << locale;
+    qDebug() << "Locale is: " << locale;
+    if (translator.load("yradio." + locale, ":/nls")) {
         app->installTranslator(&translator);
+    } else {
+        qDebug() << "Failed to load translation";
     }
 
     settings = new Settings();
-
     view->rootContext()->setContextProperty("settings", settings);
 
     view->setSource(SailfishApp::pathTo("qml/harbour-yradio.qml"));
-
     view->showFullScreen();
 
     return app->exec();
