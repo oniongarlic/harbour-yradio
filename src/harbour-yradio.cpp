18353a2f7956666d0e99e230511956b8bdd32886
Kaj-Michael Lang <milang@tal.org>
2015-11-08 19:34:55 +0200

    Bump version to 1.1.0

--- src/harbour-yradio.cpp
+++ src/harbour-yradio.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
     QTranslator translator;
     Settings *settings;
     DateUtils *dateutils;
-    const QString applicationVersion("1.0.8");
+    const QString applicationVersion("1.1.0");
 
     qmlRegisterType<FileDownloader>("harbour.yradio", 1, 0, "FileDownloader");
 
